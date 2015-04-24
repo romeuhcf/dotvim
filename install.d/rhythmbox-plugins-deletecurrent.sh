a5c1fe621e2f973531fe7fafc88503df70db2f7d
Romeu Fonseca <romeu.hcf@gmail.com>
2015-04-24 02:37:43 -0300

    Adicionando script de remocao de musica corrente do rhythmbox

---
+++ install.d/rhythmbox-plugins-deletecurrent.sh
@@ -0,0 +1,28 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: rhythmbox-plugins-deletecurrent.sh
+# 
+#         USAGE: ./rhythmbox-plugins-deletecurrent.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: YOUR NAME (), 
+#  ORGANIZATION: 
+#       CREATED: 24-04-2015 02:28
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+set -e 
+sudo apt-get install python3-xlib
+mkdir -p ~/.local/share/rhythmbox/plugins/ 
+cd ~/.local/share/rhythmbox/plugins/ 
+git clone https://github.com/ppipada/rhythmbox-plugins.git || (cd rhythmbox-plugins && git pull)
+cp -rf rhythmbox-plugins/* . -v 
+
+
