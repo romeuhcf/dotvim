332bec1d5647e4bab1e765f69cf6c1c429f7068b
Romeu Fonseca <romeu.hcf@gmail.com>
2015-05-05 18:31:53 -0300

    Adding data science tools

---
+++ install.d/data_science.sh
@@ -0,0 +1,27 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: data_science.sh
+# 
+#         USAGE: ./data_science.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: YOUR NAME (), 
+#  ORGANIZATION: 
+#       CREATED: 05/05/2015 18:26
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+set -e 
+sudo apt-get install r-base
+cd /tmp 
+wget -c -nc http://download1.rstudio.org/rstudio-0.98.1103-amd64.deb
+sudo dpkg -i rstudio-0.98.1103-amd64.deb 
+
+
