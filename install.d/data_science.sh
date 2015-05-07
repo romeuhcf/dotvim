b72ab0cd45e5eba4c71371eed0291c5cd66db92c
Romeu Fonseca <romeu.hcf@gmail.com>
2015-05-07 20:51:09 -0300

    R from rutter with java

--- install.d/data_science.sh
+++ install.d/data_science.sh
@@ -19,7 +19,18 @@
 
 set -o nounset                              # Treat unset variables as an error
 set -e 
-sudo apt-get install r-base
+sudo add-apt-repository ppa:marutter/rrutter
+sudo apt-get update
+sudo apt-get install curl
+sudo apt-get install r-base-core r-base-dev
+
+sudo apt-get install r-cran-plyr r-cran-xml r-cran-reshape r-cran-reshape2 r-cran-rmysql
+sudo apt-get install openjdk-7-*
+sudo apt-get install r-cran-rjava
+
+update-alternatives --config java
+sudo R CMD javareconf
+
 cd /tmp 
 wget -c -nc http://download1.rstudio.org/rstudio-0.98.1103-amd64.deb
 sudo dpkg -i rstudio-0.98.1103-amd64.deb 
